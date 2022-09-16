72cec91120cf6fa8214600ccd32c79bb804842a8
devmustroc <@Ismail251217>
2022-09-16 14:52:17 +0200

    status api route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,25 @@
 
 from flask import Flask, jsonify
 from api.v1.views import app_views
+from models import storage
 
 
 @app_views.route('/status', methods=["GET"])
 def status():
     """Return /status api route"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/status", methods=["GET"])
+def stats():
+    """Return status api route"""
+    data = {
+        "amenities": 47,
+        "cities": 36,
+        "places": 154,
+        "reviews": 718,
+        "states": 27,
+        "users": 31
+    }
+    data = {ks : storage.count(val) for ks, val in data.items()}
+    return jsonify(data)
