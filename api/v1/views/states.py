738af286dfb2e4810db967c1875fd76fa2934676
devmustroc <@Ismail251217>
2022-09-16 15:24:38 +0200

    CRUD FOR STATE

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-from flask import Flask, request, jsonify, abort
-from api.v1.views import app_views
+from flask import jsonify, abort, request
+
 from models import storage
 
 
@@ -9,7 +9,18 @@ def getstate(state):
     """Get state"""
     if not state:
         abort(404)
-    return (jsonify(state.to_dict(), 200)
+    return (jsonify(state.to_dict()), 200)
 
 
-
+def pustate(state):
+    """update state"""
+    if not state:
+        abort(404)
+    if not request.is_json():
+        abort(404, "Not a JSON")
+    new_req = request.get_json()
+    for (ks, val) in new_req.items():
+        if ks != "id" and ks != "updated_at" and ks != "created_at":
+            setattr(state, ks, val)
+    storage.save()
+    return jsonify(state.to_dict(), 200)
