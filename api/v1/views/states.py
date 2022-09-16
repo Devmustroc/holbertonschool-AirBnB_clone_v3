dc1ce54028374a8b22ef0a25f8adee65c48d663c
devmustroc <@Ismail251217>
2022-09-16 15:51:28 +0200

    CRUD FOR STATE

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,9 @@
 
 from flask import jsonify, abort, request
 
+from api.v1.views import app_views
 from models import storage
+from models import state
 
 
 def getstate(state):
@@ -12,7 +14,7 @@ def getstate(state):
     return (jsonify(state.to_dict()), 200)
 
 
-def pustate(state):
+def update_state(state):
     """update state"""
     if not state:
         abort(404)
@@ -24,3 +26,37 @@ def pustate(state):
             setattr(state, ks, val)
     storage.save()
     return jsonify(state.to_dict(), 200)
+
+
+def state_delete(state):
+    """delete state"""
+    if not state:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return (jsonify({}), 200)
+
+
+app_views.route("/states", methods=["GET", "POST"])
+
+
+def states():
+    """Retrieves list of all state objs or creates a state"""
+    if request.method == "GET":
+        all_states = [st.to_dict() for st in storage.all('state').values()]
+        return (jsonify(all_states), 200)
+    elif request.method == "POST":
+        if not request.is_json:
+            abort(400, "NOT a JSON")
+        new_JSon = request.get_json()
+        if 'name' not in new_JSon.keys():
+            abort(400, "Missing name")
+        sT = state()
+        for (ks, val) in new_JSon.items():
+            setattr(sT, ks, val)
+        sT.save()
+        return (jsonify(sT.to_dict(), 201)
+
+@app_views.route("/states/<ident>", methods=["GET", "PUT", "DELETE"])
+
+
