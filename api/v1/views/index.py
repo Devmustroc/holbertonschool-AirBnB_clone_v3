43e5d4789642c56305f35794479d8280c66c6c09
devmust <@Ismail251217>
2022-09-18 15:21:52 +0200

    argument implement

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
 """return JSON status on object app_views"""
 
-from flask import jsonify
-
 from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', methods=["GET"])
+@app_views.route('/status')
 def status():
-    """Return /status api route"""
-    return jsonify({"status": "OK"})
+    """ Returns status message in JSON
+    """
+    return ({"status": "OK"})
 
 
-@app_views.route("/stats", methods=["GET"])
+@app_views.route('/stats')
 def stats():
-    """Return status api route"""
-    data = {
-        "amenities": 47,
-        "cities": 36,
-        "places": 154,
-        "reviews": 718,
-        "states": 27,
-        "users": 31
-    }
-    data = {ks: storage.count(val) for ks, val in data.items()}
-    return jsonify(data)
+    """ Returns stats message in JSON
+    """
+    amens = storage.count("Amenity")
+    cities = storage.count("City")
+    places = storage.count("Place")
+    reviews = storage.count("Review")
+    states = storage.count("State")
+    users = storage.count("User")
+
+    return ({"amenities": amens,
+             "cities": cities,
+             "places": places,
+             "reviews": reviews,
+             "states": states,
+             "users": users})
+
